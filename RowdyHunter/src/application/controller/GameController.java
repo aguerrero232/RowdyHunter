a11746be89812ba1300c239570128decbe871ba9
vrixl <arielguerrero1012@gmail.com>
2019-11-09 11:52:15 -0600

    Started on the game controller.. Able to get the X and Y coordinate of the mouse clicked
    need to add in the moving space ships and their hit boxes as well as a bullet and a magazine
    I also need to keep track of where the hit box of the alien is...

--- RowdyHunter/src/application/controller/GameController.java
+++ RowdyHunter/src/application/controller/GameController.java
@@ -1,4 +1,59 @@
 package application.controller;
 
-public class GameController {
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+
+
+    @FXML
+    private AnchorPane gamepane;
+
+    @FXML
+    private ImageView gamebackground, bushesimage, groundimage, treeimage, ufoimage;
+
+    @FXML
+    private StackPane gamestackpane1;
+
+
+    public void gunshot(MouseEvent mouseEvent) {
+        // -----------------------------  getting the location of the current press of the mouse! ----------------
+        ArrayList<Double> locations = new ArrayList<Double>();
+        locations.add(mouseEvent.getSceneX());
+        locations.add(mouseEvent.getSceneY());
+        System.out.println(locations.get(0) + ", " + locations.get(1));
+        // -----------------------------  And im printing it out -------------------------------------------------
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+        // ------------------------------- GAME SCREEN IMAGES BEING SET -----------------------------------------------
+        try {
+            gamebackground.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/nightback.png")));
+            bushesimage.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/meadow.png")));
+
+//            maintitleimage2.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/titlescreentitle2-1.png")));
+//            playbuttonimage.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/homescreenplay.gif")));
+//            mainbackgroundimage.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/mainbackground.gif")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // ------------------------------- GAME SCREEN IMAGES BEING SET -----------------------------------------------
+
+
+    }
 }
