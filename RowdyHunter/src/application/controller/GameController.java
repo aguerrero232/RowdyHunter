4a155a239e02dc20828252b681643ec3bc8d6c0e
vrixl <arielguerrero1012@gmail.com>
2019-11-09 23:48:53 -0600

    added count of bullets and reloads till end game condition

--- RowdyHunter/src/application/controller/GameController.java
+++ RowdyHunter/src/application/controller/GameController.java
@@ -1,11 +1,15 @@
 package application.controller;
 
+import application.Main;
 import javafx.animation.Animation;
 import javafx.animation.ScaleTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Bounds;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -15,6 +19,7 @@ import javafx.util.Duration;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -31,12 +36,11 @@ public class GameController implements Initializable {
     @FXML
     private StackPane gamestackpane1;
 
-    private int bulletCount = 3, hitcount = 0;
+    private int bulletCount = 3, hitcount = 0, reloads = 0, score = 0;
 
 
+    public void gunshot(MouseEvent mouseEvent) throws IOException {
 
-
-    public void gunshot(MouseEvent mouseEvent) {
         // -----------------------------  getting the location of the current press of the mouse! ----------------
         ArrayList<Double> locations = new ArrayList<Double>();
         locations.add(mouseEvent.getSceneX());
@@ -44,15 +48,55 @@ public class GameController implements Initializable {
         // -----------------------------  And im checking if the pane is clicked on -------------------------------------------------
         Bounds boundsInScene = gamestackpane1.localToScene(gamestackpane1.getBoundsInLocal());
         if (boundsInScene.contains(locations.get(0), locations.get(1))) {
-            System.out.println(locations.get(0) + ", " + locations.get(1));
-            System.out.println("it works im cool");
+            System.out.println("HIT AT :" + locations.get(0) + ", " + locations.get(1));
+            score += 1;
+            System.out.println("CURRENT SCORE:" + score);
+        }
+        // -----------------------------  And im printing out the location (x,y) if true just so you can see the logic -------------------------------------------------
+
+
+        // ------------------------------------- logic conditions -------------------------------------------------
+
+
+        // -------------------- keeping track of the bullets and the number of reloads ----------------------------
+        bulletCount--;
+        System.out.println("BULLETS:" + bulletCount);
+        // update bullets display like in project 4
+        if (bulletCount == 0) {
+
+            reloads += 1;
+            // update bullets on display
+            System.out.println("Reload #:" + reloads);
+            System.out.println("Reloading....");
+
+            if (reloads >= 5) {
+                System.out.println("Game over condition.... whatever you want it to be .... i chose after 5 reloads its game over");
+                changeScene();
+            } else {
+                bulletCount = 3;
+            }
 
 
         }
-        // -----------------------------  And im printing it out the location if true -------------------------------------------------
+
     }
 
+    /**
+     * Name: changeScene()
+     * Description: changes the scene from zone to main
+     *
+     * @throws IOException
+     */
+    private void changeScene() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/Main.fxml"));
+        Main.tmpstage.setScene(new Scene(root, 900, 600));
+        Main.tmpstage.show();
+        Main.tmpstage.setResizable(false);
+    }
+
+
     private void startUFO() {
+
         // ------------------------------- moving ufo stuff -----------------------------------------------
         try {
             gamestackpane1 = new StackPane(new ImageView(new Image(new FileInputStream("RowdyHunter/resources/images/ufo-1.gif"))));
@@ -79,11 +123,8 @@ public class GameController implements Initializable {
 
     }
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
 
         // ------------------------------- GAME SCREEN IMAGES BEING SET -----------------------------------------------
         try {
