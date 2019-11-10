181f6e71a22b27564c583b7c33581764588519b4
vrixl <arielguerrero1012@gmail.com>
2019-11-10 15:47:58 -0600

    added in ufo spawn location accessible with the spawnLocations HashMap added in sounds for lazers and end game conditions

--- RowdyHunter/src/application/controller/GameController.java
+++ RowdyHunter/src/application/controller/GameController.java
@@ -13,13 +13,17 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -35,43 +39,60 @@ public class GameController implements Initializable {
     private StackPane gamestackpane1;
 
     private int bulletCount = 3, hitcount = 0, reloads = 0, score = 0;
-    private Duration tmpDuration = Duration.millis(1000);
+    HashMap<Integer, ArrayList<Integer>> spawnLocations = new HashMap<Integer, ArrayList<Integer>>();
+    private Duration tmpDuration = Duration.millis(900);
 
     public void gunshot(MouseEvent mouseEvent) throws IOException {
+
+
+        // ----------------------------- gunshot noise -------------------------------------------------
+        Random ran = new Random();
+        int gunNoiseOption = ran.nextInt(100) + 1;
+
+//        System.out.println(gunNoiseOption);
+        if (gunNoiseOption % 2 == 0) {
+            Media m = new Media(new File("RowdyHunter/resources/sounds/laser2.wav").toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(m);
+            mediaPlayer.setAutoPlay(true);
+        } else {
+            Media n = new Media(new File("RowdyHunter/resources/sounds/laser.wav").toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(n);
+            mediaPlayer.setAutoPlay(true);
+        }
+
+
+        // ---------------------------------------------------------------------';l------------------------
+
         // -----------------------------  getting the location of the current press of the mouse! ----------------
         ArrayList<Double> locations = new ArrayList<Double>();
         locations.add(mouseEvent.getSceneX());
         locations.add(mouseEvent.getSceneY());
 
         // ----------------------------- checking if the pane (the UFO) is clicked on -------------------------------------------------
-        // -----------------------------  And printing out the location (x,y) if true just so you can see the logic -------------------------------------------------
+        // -----------------------------  And printing out the location (x,y) if true just so you can see the logic -------------------
         Bounds boundsInScene = gamestackpane1.localToScene(gamestackpane1.getBoundsInLocal());
         if (boundsInScene.contains(locations.get(0), locations.get(1))) {
-            System.out.println("HIT AT :" + locations.get(0) + ", " + locations.get(1));
+            //            System.out.println("HIT AT :" + locations.get(0) + ", " + locations.get(1));
             // could add an alien animation here of one falling and fading away using a fade transition , scale transition and transition transition
+
 
             ufoExplosion(gamestackpane1.getLayoutX(), gamestackpane1.getLayoutY());
             gamepane.getChildren().remove(gamestackpane1);
 
+
             score += 1;
-            System.out.println("CURRENT SCORE:" + score);
+            //            System.out.println("CURRENT SCORE:" + score);
 
             if (score == 3) {
-                tmpDuration = Duration.millis(850);
+                tmpDuration = Duration.millis(700);
             }
             if (score == 5) {
-                tmpDuration = Duration.millis(750);
-            }
-            if (score == 8) {
                 tmpDuration = Duration.millis(600);
             }
-            if (score == 13) {
-                tmpDuration = Duration.millis(450);
-            }
-            if (score == 15) {
+            if (score == 8) {
                 tmpDuration = Duration.millis(300);
             }
-            if (score == 18) {
+            if (score == 13) {
                 tmpDuration = Duration.millis(150);
             }
 
@@ -91,6 +112,34 @@ public class GameController implements Initializable {
             //            System.out.println("Reload #:" + reloads);
             //            System.out.println("Reloading....");
             if (reloads > 8) {
+                Random random = new Random();
+                int gameOverMusic = random.nextInt(3) + 1;
+                Media media;
+
+                if (score > 10) {
+                    switch (gameOverMusic - 1) {
+                        case 1:
+                            media = new Media(new File("RowdyHunter/resources/sounds/winner.wav").toURI().toString());
+                            break;
+                        default:
+                            media = new Media(new File("RowdyHunter/resources/sounds/winner2.wav").toURI().toString());
+                    }
+                } else {
+                    switch (gameOverMusic) {
+                        case 2:
+                            media = new Media(new File("RowdyHunter/resources/sounds/gameover2.wav").toURI().toString());
+                            break;
+                        case 1:
+                            media = new Media(new File("RowdyHunter/resources/sounds/gameover.wav").toURI().toString());
+                            break;
+                        default:
+                            media = new Media(new File("RowdyHunter/resources/sounds/gameover3.wav").toURI().toString());
+                    }
+                }
+
+                MediaPlayer mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.setAutoPlay(true);
+
                 System.out.println("Game over condition.... whatever you want it to be .... i chose after 5 reloads its game over");
                 changeScene();
             } else {
@@ -116,7 +165,8 @@ public class GameController implements Initializable {
 
 
     private void ufoExplosion(double x, double y) throws FileNotFoundException {
-
+        // need an explosion sound here
+        // and possibly if we can find it then an alien with a parachute
         Image image1 = new Image(new FileInputStream("RowdyHunter/resources/images/explosion.gif"));
         ImageView imageView = new ImageView();
         imageView.setX(x);
@@ -130,30 +180,55 @@ public class GameController implements Initializable {
     }
 
 
-    private void startUFO(Duration d) {
+    private ArrayList<Integer> getAList(int a, int b) {
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+        tmp.add(a);
+        tmp.add(b);
+        return tmp;
+    }
 
+    private void setSpawnLocations() {
+        spawnLocations.put(0, getAList(200, 150));
+        spawnLocations.put(1, getAList(300, 20));
+        spawnLocations.put(2, getAList(350, 140));
+        spawnLocations.put(3, getAList(470, 100));
+        spawnLocations.put(4, getAList(100, 130));
+        spawnLocations.put(5, getAList(200, 170));
+        spawnLocations.put(6, getAList(50, 120));
+        spawnLocations.put(7, getAList(350, 220));
+        spawnLocations.put(8, getAList(432, 133));
+        spawnLocations.put(9, getAList(211, 150));
+        spawnLocations.put(10, getAList(400, 200));
+
+    }
+
+    private void startUFO(Duration d) {
         Random rand = new Random();
-        int paneX = rand.nextInt(400) + 1, paneY = rand.nextInt(300) + 1;
-        System.out.println("PANE X: " + paneX + " PANE Y: " + paneY);
+        int paneX = rand.nextInt(400) + 1, paneY = rand.nextInt(300) + 1, spawnLocation = rand.nextInt(spawnLocations.size()), setToXLocation = rand.nextInt(300) + 201, setToYLocation = rand.nextInt(100) + 51;
+
+        //                System.out.println("X: " + spawnLocations.get(spawnLocation).get(0) + ", Y: " + spawnLocations.get(spawnLocation).get(1));
+        //                System.out.println("X: " + paneX + ", Y: " + paneY);
+
         // ------------------------------- moving ufo stuff -----------------------------------------------
         try {
             gamestackpane1 = new StackPane(new ImageView(new Image(new FileInputStream("RowdyHunter/resources/images/ufo-1.gif"))));
-            gamestackpane1.setLayoutX(paneX);
-            gamestackpane1.setLayoutX(paneY);
+            gamestackpane1.setLayoutX(spawnLocations.get(spawnLocation).get(0));
+            gamestackpane1.setLayoutY(spawnLocations.get(spawnLocation).get(1));
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
         // DIFFERENT TYPES OF TRANSITIONS
         TranslateTransition transition = new TranslateTransition();
         transition.setDuration(d);
-        transition.setToX(600 + paneX);
-        transition.setToY(400 + paneY);
+        transition.setToX(setToXLocation);
+        transition.setToY(setToYLocation);
         transition.setAutoReverse(true);
         transition.setCycleCount(Animation.INDEFINITE);
-        transition.byXProperty().toString();
         transition.setNode(gamestackpane1);   // <--- SETTING THE OBJECT TO TRANSITION
         transition.play();
-        ScaleTransition sctransition = new ScaleTransition(Duration.seconds(5), gamestackpane1);
+        ScaleTransition sctransition = new ScaleTransition(Duration.seconds(1), gamestackpane1);
+        sctransition.setAutoReverse(true);
         sctransition.setToX(.2);
         sctransition.setToY(.3);
         sctransition.setCycleCount(Animation.INDEFINITE);
@@ -165,6 +240,13 @@ public class GameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // need to get this here to work .. its the games music
+        //        Media media = new Media(new File("RowdyHunter/resources/sounds/gamemusic.mp3").toURI().toString());
+        //        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        //        mediaPlayer.setAutoPlay(true);
+        //        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+
+        setSpawnLocations();
         // ------------------------------- GAME SCREEN IMAGES BEING SET -----------------------------------------------
         try {
             gamebackground.setImage(new Image(new FileInputStream("RowdyHunter/resources/images/nightback.png")));
@@ -173,6 +255,6 @@ public class GameController implements Initializable {
             e.printStackTrace();
         }
         // ------------------------------- GAME SCREEN IMAGES BEING SET -----------------------------------------------
-        startUFO(Duration.seconds(2));
+        startUFO(Duration.seconds(1));
     }
 }
