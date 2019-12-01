b7c12e2994686947ede151911370198301feb407
Polaris81 <55464461+Polaris81@users.noreply.github.com>
2019-11-30 23:08:02 -0600

    Added comments and closed open readers and writers

--- RowdyHunter/src/application/model/HighScores.java
+++ RowdyHunter/src/application/model/HighScores.java
@@ -1,4 +1,6 @@
 /**
+ * HighScores.java
+ * 
  * @Author Ariel Guerrero
  */
 
@@ -10,20 +12,43 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class HighScores implements Comparable<HighScores> {
-
+    /**
+	 * Holds score value
+	 */
     private int score = 0;
+    /**
+     * Holds user's name
+     */
     private String username = "";
+    /**
+     * ArrayList to hold HighScore objects
+     */
     private ArrayList<HighScores> scores = new ArrayList<HighScores>();
-
+    /**
+     * Constructor
+     * @param score
+     * 			The score value passed in
+     * @param username
+     * 			The name value passed in
+     * @throws IOException
+     */
     public HighScores(int score, String username) throws IOException {
         this.score = score;
         this.username = username;
     }
-
+    /**
+     *Get the list of HighScore objects
+     * @return
+     */
     public ArrayList<HighScores> getScores() {
         return scores;
     }
-
+    /**
+     * Create a new HighScore object from string input
+     * @param line
+     * @return
+     * @throws IOException
+     */
     public HighScores getScoreFromLine(String line) throws IOException {
         String[] vars = new String[2];
         Scanner scan = new Scanner(line);
@@ -31,9 +56,13 @@ public class HighScores implements Comparable<HighScores> {
         for (int i = 0; i < vars.length; i++)
             vars[i] = scan.next();
 
+        scan.close();
         return new HighScores(Integer.valueOf(vars[0]), vars[1]);
     }
-
+    /**
+     * Load score values from file
+     * @throws IOException
+     */
     public void loadScores() throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("RowdyHunter/resources/data/highscores.csv")));
         String line = "";
@@ -45,8 +74,14 @@ public class HighScores implements Comparable<HighScores> {
         }
         Collections.sort(scores);
         scores = getTop5(scores);
+        reader.close();
     }
-
+    /**
+     * Pull the top 5 score values from a HighScore arraylist
+     * @param tmp
+     * @return
+     * 			The top 5 scores in a new HighScore arraylist
+     */
     private ArrayList<HighScores> getTop5(ArrayList<HighScores> tmp) {
         ArrayList<HighScores> nottmp = new ArrayList<HighScores>();
         for (int i = 0; i < 5; i++) {
@@ -56,28 +91,46 @@ public class HighScores implements Comparable<HighScores> {
         }
         return nottmp;
     }
-
+    /**
+     * Get username
+     * @return
+     */
     public String getUsername() {
         return username;
     }
-
+    /**
+     * Set the username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    /**
+     * Get score value
+     * @return
+     */
     public int getScore() {
         return score;
     }
-
+    /**
+     * Set the score value
+     * @param score
+     */
     public void setScore(int score) {
         this.score = score;
     }
-
+    /**
+     * Overridden toString to display scores
+     */
     @Override
     public String toString() {
         return getScore() + "," + getUsername() + "\n";
     }
-
+    /**
+     * Read scores from file, add current score to values and write values to file
+     * @param score
+     * @throws IOException
+     */
     public void writeScoreToFile(int score) throws IOException {
         HighScores test = new HighScores(score, this.username), tmp;
         BufferedReader reader = new BufferedReader(new FileReader(new File("RowdyHunter/resources/data/highscores.csv")));
@@ -94,6 +147,7 @@ public class HighScores implements Comparable<HighScores> {
             scores.add(tmp);
         }
 
+        reader.close();
         Collections.sort(scores);
 
         while (scores.size() > 5)
@@ -106,7 +160,9 @@ public class HighScores implements Comparable<HighScores> {
         writer.write(output);
         writer.close();
     }
-
+    /**
+     * Comparator for array sorting
+     */
     public int compareTo(HighScores other) { return other.score - this.score; }
 
 }
