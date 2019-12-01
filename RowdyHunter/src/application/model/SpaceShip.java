0bcae1de1442f95d351f0abaea257ecd082c4900
Polaris81 <55464461+Polaris81@users.noreply.github.com>
2019-11-30 23:21:17 -0600

    Added comments

--- RowdyHunter/src/application/model/SpaceShip.java
+++ RowdyHunter/src/application/model/SpaceShip.java
@@ -1,4 +1,6 @@
 /**
+ * SpaceShip.java
+ * 
  * @Author Ariel Guerrero
  */
 
@@ -12,19 +14,37 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class SpaceShip extends StackPane {
+    /**
+	 * String to hold path to image location
+	 */
     String imageUrl = "";
+    /**
+     * IamgeView object to hold image for displaying
+     */
     ImageView ufoimage = null;
-
+    /**
+     * Constructor
+     * Sets a ufo image to the IamgeView ufoimage and adds it to itself(a StackPane)
+     * @param imageUrl
+     * 				passed in image location string
+     * @throws FileNotFoundException
+     */
     public SpaceShip(String imageUrl) throws FileNotFoundException {
         this.imageUrl = imageUrl;
         setUfoimage(new ImageView(new Image(new FileInputStream(imageUrl))));
         getChildren().add(getUfoimage());
     }
-
+    /**
+     * Get the ImageView object for the class
+     * @return
+     */
     public ImageView getUfoimage() {
         return ufoimage;
     }
-
+    /**
+     * Set the ImageView object for the class
+     * @param ufoimage
+     */
     public void setUfoimage(ImageView ufoimage) {
         this.ufoimage = ufoimage;
     }
